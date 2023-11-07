cd4661a9db11d48ad830a34a812ac3641a120e9a
Suneet Jain <suneet.bhopal@gmail.com>
2023-11-07 13:42:57 -0500

    Login Logout Backend

--- app.py
+++ app.py
@@ -1,18 +1,50 @@
-from flask import Flask, jsonify
-import cx_Oracle
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+import os
+import oracledb
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
-# Set up DSN (Data Source Name)
-dsn = cx_Oracle.makedsn('HOST', 'PORT', sid='YOUR_SID')
+load_dotenv()
+
+app.secret_key = os.getenv('SECRET_KEY') # 256-bit encryption 
+
+# # Set up DSN (Data Source Name)
+# dsn = cx_Oracle.makedsn(os.getenv('DB_HOST'), os.getenv('DB_PORT'), sid=os.getenv('DB_SID'))
 
 # Establish the database connection
-# connection = cx_Oracle.connect(user='USERNAME', password='PASSWORD', dsn=dsn)
+connection = oracledb.connect(user=os.getenv('DB_USERNAME'), password=os.getenv('DB_PASSWORD'), dsn=os.getenv('DB_HOST') + "/" + os.getenv('DB_SID'))
 
 
 @app.route('/')
 def home():
     return 'Welcome to DBMS Project'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Validation From SQL here
+        if username == 'admin' and password == 'password':
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid credentials', 'danger')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        #Email Verification here
+
+        flash('Registration successful, please login.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)

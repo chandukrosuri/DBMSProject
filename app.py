4ac78eae9de1632f55858fa2f88e167d89b7fed6
Suneet Jain <suneet.bhopal@gmail.com>
2023-11-07 09:20:40 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+import cx_Oracle
+
+app = Flask(__name__)
+
+# Set up DSN (Data Source Name)
+dsn = cx_Oracle.makedsn('HOST', 'PORT', sid='YOUR_SID')
+
+# Establish the database connection
+# connection = cx_Oracle.connect(user='USERNAME', password='PASSWORD', dsn=dsn)
+
+
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)

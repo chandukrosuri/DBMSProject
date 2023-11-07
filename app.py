d76246ad52531874b96cd6c8622e4f9eb1dc9d05
Suneet Jain <suneet.bhopal@gmail.com>
2023-11-07 09:24:47 -0500

    First Commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import cx_Oracle
 app = Flask(__name__)
 
 # Set up DSN (Data Source Name)
-dsn = cx_Oracle.makedsn('HOST', 'PORT', sid='YOUR_SID')
+# dsn = cx_Oracle.makedsn('HOST', 'PORT', sid='YOUR_SID')
 
 # Establish the database connection
 # connection = cx_Oracle.connect(user='USERNAME', password='PASSWORD', dsn=dsn)

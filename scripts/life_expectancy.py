4dee8d8fe659efb94f45ad2bd6732af5f84ee1c8
Rahul Varki <rvarki@bodhi.cs.unh.edu>
2023-11-18 20:55:26 -0500

    Updated Scripts

--- scripts/life_expectancy.py
+++ scripts/life_expectancy.py
@@ -5,21 +5,28 @@ import os
 
 data = "../data/life_expectancy.csv"
 sql = "../sql_commands/life_expectancy.sql"
+country_sql = "../sql_commands/life_expectancy_country.sql"
+year_sql = "../sql_commands/life_expectancy_year.sql"
 
 TABLE_NAME = "LIFE_EXPECTANCY"
 
 # Create .sql file for life expectancy
 with open(data, 'r') as infile:
     with open(sql, 'w') as outfile:
-        csv_reader = csv.reader(infile)
-        # Skip the first two header rows
-        next(csv_reader)
-        next(csv_reader)
-        for row in csv_reader:
-            COUNTRY = row[0].split("(")[0]
-            YEAR = row[1]
-            BOTH_SEXES = row[2] if len(row[2]) != 0 else "NULL"
-            MALE = row[3] if len(row[3]) != 0 else "NULL"
-            FEMALE = row[4] if len(row[4]) != 0 else "NULL"
-            outfile.write("INSERT INTO " + TABLE_NAME + " VALUES (" + COUNTRY + "," + YEAR + "," + BOTH_SEXES + "," + MALE + "," + FEMALE + ");" + "\n")
-           
+         with open(country_sql, 'w') as countryfile:
+            with open(year_sql, 'w') as yearfile:
+                csv_reader = csv.reader(infile)
+                # Skip the first two header rows
+                next(csv_reader)
+                next(csv_reader)
+                hiID = 0
+                for row in csv_reader:
+                    COUNTRY = row[0].split("(")[0].replace("'","")
+                    YEAR = row[1]
+                    BOTH_SEXES = row[2] if len(row[2]) != 0 else "NULL"
+                    MALE = row[3] if len(row[3]) != 0 else "NULL"
+                    FEMALE = row[4] if len(row[4]) != 0 else "NULL"
+                    outfile.write("INSERT INTO " + TABLE_NAME + " VALUES (" + str(hiID) + "," + "'" + COUNTRY + "'" + "," + YEAR + "," + BOTH_SEXES + "," + MALE + "," + FEMALE + ");" + "\n")
+                    countryfile.write("INSERT INTO " + "COUNTRY" + " VALUES (" + "'" + COUNTRY + "'" + ");" + "\n")
+                    yearfile.write("INSERT INTO " + "TIME_YEAR" + " VALUES (" + YEAR + ");" + "\n")
+                    hiID += 1
